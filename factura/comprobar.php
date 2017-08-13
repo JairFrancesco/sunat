51073c6253594673bf825f64ca0477ed9f893357
sistemas <sitemas@surmotriz.com>
2017-08-13 14:25:22 -0500

    base

--- factura/comprobar.php
+++ factura/comprobar.php
@@ -112,14 +112,14 @@ try {
     //echo $result;
     preg_match_all('/<statusCode>(.*?)<\/statusCode>/is', $result, $matches_codigo);
     preg_match_all('/<statusMessage>(.*?)<\/statusMessage>/is', $result, $matches_mensaje);
-    if($matches_codigo[1][0] == '0001'){
+    if($matches_codigo[1][0] == '0001' || $matches_codigo[1][0] == '0003'){
         echo '<img src="images/successful.jpg" width="400" height="395" style="display:block; margin:auto;" alt=""><br>';
-        echo '<div style="text-align: center;">Codigo : '.$matches_codigo[1][0].'-'.$matches_mensaje[1][0].' <strong>'.$serie.'-'.$cab_doc_gen['CDG_NUM_DOC'].'</strong>';
+        echo '<div style="text-align: center;">'.$matches_codigo[1][0].'-'.$matches_mensaje[1][0].' '.$serie.'-'.$cab_doc_gen['CDG_NUM_DOC'];
         //echo '<a href="terminar.php?gen='.$cab_doc_gen['CDG_COD_GEN'].'&emp='.$cab_doc_gen['CDG_COD_EMP'].'&tip='.$cab_doc_gen['CDG_TIP_DOC'].'&num='.$cab_doc_gen['CDG_NUM_DOC'].'"><button class="action bluebtn"><span class="label"><strong>Terminar '.$serie.'-'.$cab_doc_gen['CDG_NUM_DOC'].'</strong></span></button></a></div>';
         echo '</div>';
     }else{
         echo '<img src="images/error.png" width="400" height="395" style="display:block; margin:auto;" alt=""><br>';
-        echo '<div style="text-align: center;"><strong>'.$serie.'-'.$cab_doc_gen['CDG_NUM_DOC'].'</strong> Si el codigo sale 0001 esta bien, pero si sale otro esta mal. <br><strong>Codigo :</strong> ' . $matches_codigo[1][0] . '<br> <strong>Mensaje :</strong> ' . $matches_mensaje[1][0].'</div>';
+        echo '<div style="text-align: center;">'.$serie.'-'.$cab_doc_gen['CDG_NUM_DOC'].' '.$matches_codigo[1][0].' '.$matches_mensaje[1][0].'</div>';
     }
 }catch (Exception $e) {
     echo '<img src="images/error.png" width="400" height="395" style="display:block; margin:auto;" alt=""><br>';
