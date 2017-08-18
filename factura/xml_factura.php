5af04438fbeb2c604e0a27ee3316d88ef65a95ca
Moises <sistemas@surmotriz.com>
2017-08-18 11:34:19 -0500

    base

--- factura/xml_factura.php
+++ factura/xml_factura.php
@@ -43,6 +43,7 @@
     function exception_error_handler($errno, $errstr, $errfile, $errline ) {
         throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
     }
+
     set_error_handler("exception_error_handler");
 
     /* PARAMETROS GET
