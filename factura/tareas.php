3f26fade9fbb084b9ce453127452e9635434ef1a
sistemas <sitemas@surmotriz.com>
2017-10-01 19:53:32 -0500

    base

---
+++ factura/tareas.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- bootstrap 3 -->
+	<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">	
+	<script src="../bootstrap/js/jquery.min.js"></script>
+	<script src="../bootstrap/js/bootstrap.js"></script>
+<body>
+	<?php 
+		include "layout/__auth.php";
+		include "layout/__nav_bar.php";
+	?>
+
+</body>
+
+</html>
