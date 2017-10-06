f92459bf6c13791f691a2e1ded48b582abd2f3e6
sistemas <sistemas@surmotriz.com>
2017-10-06 16:07:48 +0000

    base

--- factura/conexion.php
+++ factura/conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = oci_connect("dti", "dti", "54.202.12.82/xe",'AL32UTF8');
+    $conn = oci_connect("dti", "dti", "172.31.35.251/xe",'AL32UTF8');
     //54.202.12.82
     //192.168.1.35
 ?>
