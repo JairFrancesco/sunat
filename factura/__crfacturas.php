872e30657d724553491492cf45d33f1875f7ab39
sistemas <sitemas@surmotriz.com>
2017-10-10 16:05:42 -0500

    base

--- factura/__crfacturas.php
+++ factura/__crfacturas.php
@@ -1,31 +1,22 @@
 <?php
 
-    $gen = '02';
-    $emp = '01';
-    $fecha = '09-10-2017';
+$gen = '02';
+$fecha = date("d-m-Y");
 
-    /*Conexion
-    ******************/
-    include "conexion.php";
+/*Consulta Facturas
+**********************/
+$sql_facturas_dia = "select * from cab_doc_gen where cdg_tip_doc='F' and to_char(cdg_fec_gen,'dd-mm-yyyy') = '".$fecha."' and cdg_cod_emp='".$emp."' and cdg_cod_gen='".$gen."'";
+$sql_parse = oci_parse($conn,$sql_facturas_dia);
+oci_execute($sql_parse);
+oci_fetch_all($sql_parse, $facturas, null, null, OCI_FETCHSTATEMENT_BY_ROW); //sin array numeros
 
-    /*Consulta Facturas
-    **********************/
-    $sql_facturas_dia = "select * from cab_doc_gen where to_char(cdg_fec_gen,'dd-mm-yyyy')='09-10-2017' and cdg_cod_emp='01' 
-      and cdg_tip_doc in ('A') and  cdg_tip_ref in ('FS','FR','FC')
-      union 
-      select * from cab_doc_gen where to_char(cdg_fec_gen,'dd-mm-yyyy')='09-10-2017' and cdg_cod_emp='01' 
-      and cdg_tip_doc in ('F')";
-    $sql_parse = oci_parse($conn,$sql_facturas_dia);
-    oci_execute($sql_parse);
-    oci_fetch_all($sql_parse, $documentos, null, null, OCI_FETCHSTATEMENT_BY_ROW); //sin array numeros
-
-
-    //$rfacturas=1;
-    foreach ($documentos as $documento){
-
+$rfacturas=1;
+foreach ($facturas as $factura){
+    if ($factura['CDG_COD_SNT'] ==''){
+        $rfacturas=1;
     }
-
-    //echo $rfacturas;
-    //print_r($facturas);
+}
+//echo $rfacturas;
+//print_r($facturas);
 
 ?>
