b2425a6d379f84450162ce377bc61fbd986dd09f
Moises <sistemas@surmotriz.com>
2017-10-12 16:15:53 -0500

    base

---
+++ factura/apis/__comprobar_facturas.php
@@ -0,0 +1,55 @@
+<?php
+    if (($cod != '0001' && $cod != '0003') || ($anu_sn=='S' && $doc_anu=='S' && $cod != '0003')){
+        /* DOC Y SERIE 01-F001
+        *******************/
+        if ($tip == 'F') {
+            $doc = '01';
+            $serie = 'F00' . $ser;
+        } elseif ($tip == 'B') {
+            $doc = '03';
+            $serie = 'B00' . $ser;
+        } elseif ($tip == 'A') {
+            $doc = '07';
+            $serie = 'FN0' . $ser;
+
+        }
+
+        $wsdlURL = 'https://www.sunat.gob.pe/ol-it-wsconscpegem/billConsultService?wsdl';
+        $XMLString = '<soapenv:Envelope xmlns:ser="http://service.sunat.gob.pe" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+        <soapenv:Header>
+        <wsse:Security>
+        <wsse:UsernameToken>
+        <wsse:Username>20532710066SURMOTR1</wsse:Username>
+        <wsse:Password>TOYOTA2051</wsse:Password>
+        </wsse:UsernameToken>
+        </wsse:Security>
+        </soapenv:Header>
+        <soapenv:Body>
+        <ser:getStatus>
+        <rucComprobante>20532710066</rucComprobante>
+        <tipoComprobante>'.$doc.'</tipoComprobante>
+        <serieComprobante>'.$serie.'</serieComprobante>
+        <numeroComprobante>'.$num.'</numeroComprobante>
+        </ser:getStatus>
+        </soapenv:Body>
+        </soapenv:Envelope>';
+
+        try {
+            $result = soapCall($wsdlURL, $callFunction = "getStatusCdr", $XMLString);
+        } catch (Exception $e){
+            echo "Error en conexion con sunat, espere y vuelva a intentarlo";
+        }
+        //echo $result;
+        preg_match_all('/<statusCode>(.*?)<\/statusCode>/is', $result, $matches_codigo);
+        preg_match_all('/<statusMessage>(.*?)<\/statusMessage>/is', $result, $matches_mensaje);
+
+        if($matches_codigo[1][0] == '0001' || $matches_codigo[1][0] == '0003'){
+            include "../conexion.php";
+            $update = "update cab_doc_gen SET cdg_cod_snt='".$matches_codigo[1][0]."' WHERE cdg_num_doc='".$num."' and cdg_cla_doc='".$cla."' and cdg_cod_emp='".$emp."' and cdg_cod_gen='".$gen."'";
+            $stmt = oci_parse($conn, $update);
+            oci_execute($stmt, OCI_COMMIT_ON_SUCCESS);
+            oci_free_statement($stmt);
+        }
+    }
+
+?>
--- factura/apis/envio_facturas.php
+++ factura/apis/envio_facturas.php
@@ -1,12 +1,19 @@
 <?php
 
+    /*condigurando try cash
+    *****************************/
+    function exception_error_handler($errno, $errstr, $errfile, $errline ) {
+        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    }
+    set_error_handler("exception_error_handler");
+
     /*Config Fecha Peru
     **********************/
     date_default_timezone_set('America/Lima');
 
     /*Parametros
     **********************/
-    $fecha = '09-10-2017';
+    $fecha = '30-09-2017';
     $emp = '01';
 
     /*Conexion
@@ -34,6 +41,18 @@
         include "__xml_nota.php";
     }
 
+    /*Funcion Comprobar
+    ******************************/
+    function comprobar_facturas($tip,$ser,$num,$cod,$cla,$emp,$gen,$anu_sn,$doc_anu){
+        include "__comprobar_facturas.php";
+    }
+
+    /*Funcion Baja
+    ************************/
+    function crear_baja_factura($num){
+        include "__baja_xml_factura.php";
+    }
+
     /*Soap client
     *******************/
     include "../__soap.php";
@@ -46,7 +65,7 @@
     $sql_facturas_dia = "select * from cab_doc_gen where to_char(cdg_fec_gen,'dd-mm-yyyy')='".$fecha."' and cdg_cod_emp='".$emp."'
       and cdg_tip_doc in ('A') and  cdg_tip_ref in ('FS','FR','FC') 
       union
-      select * from cab_doc_gen where to_char(cdg_fec_gen,'dd-mm-yyyy')='09-10-2017' and cdg_cod_emp='01' and cdg_tip_doc in ('F')";
+      select * from cab_doc_gen where to_char(cdg_fec_gen,'dd-mm-yyyy')='".$fecha."' and cdg_cod_emp='01' and cdg_tip_doc in ('F')";
     $sql_parse = oci_parse($conn,$sql_facturas_dia);
     oci_execute($sql_parse);
     oci_fetch_all($sql_parse, $documentos, null, null, OCI_FETCHSTATEMENT_BY_ROW); //sin array numeros
@@ -58,15 +77,30 @@
         $crear_tip = $documento['CDG_TIP_DOC'];
         $crear_num = $documento['CDG_NUM_DOC'];
         if ($documento['CDG_TIP_DOC']=='F'){
-            crear_xml_factura($crear_gen,$crear_emp,$crear_tip,$crear_num);
+            //crear_xml_factura($crear_gen,$crear_emp,$crear_tip,$crear_num);
         }elseif ($documento['CDG_TIP_DOC']=='A'){
-            crear_xml_nota($crear_gen,$crear_emp,$crear_tip,$crear_num);
+            //crear_xml_nota($crear_gen,$crear_emp,$crear_tip,$crear_num);
         }
     }
 
     /*Comprobacion sunat cdg_cod_env=001*/
     foreach ($documentos as $documento){
-        
+        $crear_tip = $documento['CDG_TIP_DOC'];
+        $crear_ser = $documento['CDG_SER_DOC'];
+        $crear_num = $documento['CDG_NUM_DOC'];
+        $crear_cod = $documento['CDG_COD_SNT'];
+        $crear_cla = $documento['CDG_CLA_DOC'];
+        $crear_emp = $documento['CDG_COD_EMP'];
+        $crear_gen = $documento['CDG_COD_GEN'];
+        $crear_anu_sn = $documento['CDG_ANU_SN'];
+        $crear_doc_anu = $documento['CDG_DOC_ANU'];
+        //comprobar_facturas($crear_tip,$crear_ser,$crear_num,$crear_cod,$crear_cla,$crear_emp,$crear_gen,$crear_anu_sn,$crear_doc_anu);
     }
 
+    foreach ($documentos as $documento){
+        if ($documento['CDG_ANU_SN']=='S' && $documento['CDG_DOC_ANU']=='S' && $documento['CDG_COD_SNT']=='0003'){
+            $crear_num = $documento['CDG_NUM_DOC'];
+            crear_baja_factura($crear_num);
+        }
+    }
 ?>
